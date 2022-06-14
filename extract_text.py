b8008ecbd2ede4ff94e13e8178c8c36f6c886d8f
lijingyang <lijingyang@fang.com>
2022-06-14 09:55:16 +0800

    Update extract_text.py
    语法

--- extract_text.py
+++ extract_text.py
@@ -5,16 +5,15 @@ from trans_params import KAKUYOMU_PARAMS as PARAMS_DATA
 # HTML文件转为txt
 # path=要读的文件的全路径; name=文件名; write_path=写入路径
 def trans_text(path, name, write_path, params_data):
-    temp_file = open(path, encoding='utf-8', errors='ignore')
-    content = temp_file.read()
-    soup = BeautifulSoup(content, 'html.parser')
-    js_episode_body = soup.find_all('div', class_=params_data.get('TARGET_DOM_CLASS'))
-    temp_all_text = js_episode_body[0].strings
-    temp_file.close()
+    with open(path, encoding='utf-8', errors='ignore') as temp_file:
+    # temp_file = open(path, encoding='utf-8', errors='ignore')
+        content = temp_file.read()
+        soup = BeautifulSoup(content, 'html.parser')
+        js_episode_body = soup.find_all('div', class_=params_data.get('TARGET_DOM_CLASS'))
+        temp_all_text = js_episode_body[0].strings
 
-    transe_file = open('{0}/{1}.txt'.format(write_path, name), mode='w', encoding='utf-8')
-    transe_file.write(''.join(list(temp_all_text)))
-    transe_file.close()
+    with open('{0}/{1}.txt'.format(write_path, name), mode='w', encoding='utf-8') as transe_file:
+        transe_file.write(''.join(list(temp_all_text)))
 
 # 找一下文件夹在不在目标目录下，不在就创建
 def find_dir(name, params_data, path='./'):
